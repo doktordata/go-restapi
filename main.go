98f8af5c6748937c3338c93c242aa377496f0c36
Erik Palm <erik.axel.palm@gmail.com>
2019-04-28 00:11:27 +0200

    Use PostgreSQL as data store

--- main.go
+++ main.go
@@ -1,55 +1,102 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 // Vehichle Struct
 type Vehichle struct {
 	ID      string `json:"id"`
-	Make    *Make  `json:"make"`
+	Make    string `json:"make"`
 	Model   string `json:"model"`
 	Vintage string `json:"vintage"`
 }
 
 // Make Struct
-type Make struct {
-	Name string `json:"name"`
+// type Make struct {
+// 	Name string `json:"name"`
+// }
+
+var db *sql.DB
+
+func connectDb() {
+	var err error
+
+	db, err = sql.Open("postgres", "user=erik dbname=bumper sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 }
 
-var vehichles []Vehichle
-
 func getVehichles(w http.ResponseWriter, r *http.Request) {
+	var vehichles []Vehichle
+
+	rows, err := db.Query(`SELECT id, make, model, vintage FROM vehichles`)
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		vehichle := Vehichle{}
+
+		err = rows.Scan(&vehichle.ID, &vehichle.Make, &vehichle.Model, &vehichle.Vintage)
+		if err != nil {
+			panic(err)
+		}
+
+		vehichles = append(vehichles, vehichle)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(vehichles)
+	if len(vehichles) != 0 {
+		json.NewEncoder(w).Encode(vehichles)
+	} else {
+		w.WriteHeader(404)
+	}
 }
 
 func getVehichle(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
+	vehichle := Vehichle{}
 
-	for _, vehichle := range vehichles {
-		if vehichle.ID == params["id"] {
-			json.NewEncoder(w).Encode(vehichle)
+	id := mux.Vars(r)["id"]
+	row := db.QueryRow(`SELECT id, make, model, vintage FROM vehichles WHERE id=$1`, id)
+	err := row.Scan(&vehichle.ID, &vehichle.Make, &vehichle.Model, &vehichle.Vintage)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(404)
+			return
+		} else {
+			w.WriteHeader(500)
 			return
 		}
 	}
-	w.WriteHeader(404)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(vehichle)
 }
 
 func main() {
-	r := mux.NewRouter()
+	connectDb()
+	defer db.Close()
 
-	// Mock data
-	vehichles = append(vehichles, Vehichle{ID: "1", Model: "911", Vintage: "2000", Make: &Make{Name: "Porsche"}})
-	vehichles = append(vehichles, Vehichle{ID: "2", Model: "E320", Vintage: "1997", Make: &Make{Name: "Mercedes-Benz"}})
-	vehichles = append(vehichles, Vehichle{ID: "3", Model: "XM", Vintage: "1997", Make: &Make{Name: "Citroën"}})
-	vehichles = append(vehichles, Vehichle{ID: "4", Model: "924", Vintage: "1980", Make: &Make{Name: "Porsche"}})
-	vehichles = append(vehichles, Vehichle{ID: "5", Model: "E200", Vintage: "1987", Make: &Make{Name: "Mercedes-Benz"}})
+	r := mux.NewRouter()
 
 	r.HandleFunc("/api/vehichles", getVehichles).Methods("GET")
 	r.HandleFunc("/api/vehichles/{id}", getVehichle).Methods("GET")
